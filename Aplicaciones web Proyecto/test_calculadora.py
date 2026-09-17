9c9e12ef355111ec4d3429f255f50e1e6b703c40
Nicaib <nicolay.c.ibarra@gmail.com>
2026-09-16 17:42:36 -0700

    Delete Aplicaciones web Proyecto directory
    error de indentacion

--- Aplicaciones web Proyecto/test_calculadora.py
+++
@@ -1,7 +0,0 @@
-from Calculadora_simple import Funcion
-
-def test_operaciones_calculadora():
-    assert Funcion.suma(5, 3) == 8
-    assert Funcion.resta(10, 4) == 6
-    assert Funcion.multiplicacion(3, 4) == 12    
-    assert Funcion.division(10, 2) == 5
